34434286880825e9904bf518602fa56686d482e5
Leonardo Deorce <leodeorce@gmail.com>
2022-09-24 18:10:41 -0300

    Removendo dependência em "boca-web"

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+FROM node:16
+
+RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
+RUN apt-get -y update
+RUN apt-get -y install \
+    dialog \
+    apt-utils \
+    php \
+    php-pgsql \
+    php-gd \
+    postgresql-client
+RUN apt-get -y upgrade && \
+    apt-get -y clean
+
+WORKDIR /usr/src/app
+
+# Clone BOCA repository
+RUN git clone https://github.com/cassiopc/boca.git
+
+WORKDIR boca
+
+# Copy modified source code files
+RUN wget -O ./src/private/conf.php \
+    https://raw.githubusercontent.com/joaofazolo/boca-docker/master/src/private/conf.php
+RUN wget -O ./src/private/createdb.php \
+    https://raw.githubusercontent.com/joaofazolo/boca-docker/master/src/private/createdb.php
+
+WORKDIR /usr/src/app
+
+COPY package.json .
+COPY yarn.lock .
+
+RUN yarn install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["/bin/bash", "-c", "sh ./docker/init.sh ; yarn dev"]
---
+++ docker/init.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#========================================================================
+# Copyright 2020-2021 Joao Vitor Alves Fazolo and Rodrigo Laiola Guimaraes
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
+# This program is released under license GNU GPL v3+ license.
+#
+#========================================================================
+
+until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -c '\q'; do
+  >&2 echo "PostgreSQL server is unavailable - sleeping"
+  sleep 1
+done
+  
+>&2 echo "PostgreSQL server is up - executing command"
+
+# https://stackoverflow.com/questions/14549270/check-if-database-exists-in-postgresql-using-shell
+if PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -lqt | cut -d \| -f 1 | grep -qw $DB_NAME; then
+	# $? is 0
+    echo "Database already exists"
+else
+    # ruh-roh
+    # $? is 1
+    echo "Create database"
+    # https://stackoverflow.com/questions/5891888/piping-data-into-command-line-php
+
+    cd ./boca/src/private
+    echo "YES" | php createdb.php
+fi
