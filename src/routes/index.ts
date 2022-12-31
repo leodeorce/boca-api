0f7f4e749381b37c399c3bf82c736d97280de4d4
Leonardo Deorce <leodeorce@gmail.com>
2022-12-31 18:05:02 -0300

    Remoção de fluxos para casos de uso incompatíveis com o BOCA
    - "Workings" e suas relações, e "problems" que possuem "langs"

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,7 +8,6 @@ import { problemsRoutes } from "./problems.routes";
 import { runsRoutes } from "./runs.routes";
 import { sitesRoutes } from "./sites.routes";
 import { usersRoutes } from "./users.routes";
-import { workingsRoutes } from "./working.routes";
 
 const router = Router();
 
@@ -20,6 +19,5 @@ router.use("/api", problemsRoutes);
 router.use("/api", runsRoutes);
 router.use("/api", sitesRoutes);
 router.use("/api", usersRoutes);
-router.use("/api", workingsRoutes);
 
 export { router };
